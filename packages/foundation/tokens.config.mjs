20062f57924471b9f461baf7ac0f61265f7cb3eb
Christoph Fricke <christoph.fricke@holisticon.de>
2024-06-21 11:55:00 +0200

    feat(tokens): prefix tokens with "hap" automatically

--- packages/foundation/tokens.config.mjs
+++ packages/foundation/tokens.config.mjs
@@ -1,8 +1,12 @@
-import pluginCSS from "@cobalt-ui/plugin-css";
+import pluginCSS, { makeNameGenerator } from "@cobalt-ui/plugin-css";
 
 /** @type {import("@cobalt-ui/core").Config} */
 export default {
   tokens: "./atomic-playfulness.tokens.json",
   outDir: "./dist",
-  plugins: [pluginCSS()],
+  plugins: [
+    pluginCSS({
+      generateName: makeNameGenerator(undefined, "hap"),
+    }),
+  ],
 };
