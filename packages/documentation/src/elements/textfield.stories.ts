1ec5d227f0f41d31e9ef85dafe4f69c10a6d8570
Christoph Fricke <christoph.fricke@holisticon.de>
2024-08-02 14:55:37 +0200

    docs(textfield): add textfield stories

---
+++ packages/documentation/src/elements/textfield.stories.ts
@@ -0,0 +1,41 @@
+import type { Meta, StoryObj } from "@storybook/web-components";
+import { html } from "lit";
+
+interface TextfieldArgs {
+  label: string;
+  disabled: boolean;
+  required: boolean;
+}
+
+const meta: Meta<TextfieldArgs> = {
+  //TODO: I am used to having autodocs without explicitly declaring the tag... Did not find the config. How can we enable it implicitly.
+  tags: ["autodocs"],
+  args: {
+    label: "Textfield",
+    disabled: false,
+    required: false,
+  },
+  render: (args) => html`
+    <div class="hap-textfield">
+      <label>${args.label}</label>
+      <input ?required=${args.required} ?disabled=${args.disabled} />
+    </div>
+  `,
+};
+
+export default meta;
+type Story = StoryObj<TextfieldArgs>;
+
+export const Default: Story = {};
+
+export const Required: Story = {
+  args: {
+    required: true,
+  },
+};
+
+export const Disabled: Story = {
+  args: {
+    disabled: true,
+  },
+};
