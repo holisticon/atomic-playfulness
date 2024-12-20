54f9333946b7f1a9f24461b916a119175b93892e
Fabian Gaukler <fabian.gaukler@holisticon.de>
2024-12-20 18:41:41 +0100

    fix: uses transform in terrazzo to get correct values

--- packages/foundation/terrazzo.config.mjs
+++ packages/foundation/terrazzo.config.mjs
@@ -13,6 +13,29 @@ export default defineConfig({
         name = name.replace("hap-tokens", "hap");
         return name;
       },
+      transform: (token) => {
+        if (token.$type === "string") {
+          return token.$value;
+        }
+
+        // opacity values in figma range from 0 to 100
+        if (
+          token.$type === "dimension" &&
+          token.id.toLowerCase().includes("opacity")
+        ) {
+          return `${token.$value.value / 100}`;
+        }
+
+        // letter-spacing values in figma are float decimals
+        if (
+          token.$type === "dimension" &&
+          token.id.toLowerCase().includes("letter-spacing")
+        ) {
+          return Number(token.$value.value).toFixed(2);
+        }
+
+        return void 0;
+      },
     }),
   ],
 });
