5cbed76da46e0828af6fda43f4bac767e19b446c
Christoph Fricke <christoph.fricke@holisticon.de>
2025-01-17 15:53:01 +0100

    feat(textfield): add invalid styling and round styling

--- packages/documentation/src/elements/textfield/textfield.stories.ts
+++ packages/documentation/src/elements/textfield/textfield.stories.ts
@@ -1,5 +1,6 @@
 import type { Meta, StoryObj } from "@storybook/web-components";
 import { html } from "lit";
+import { classMap } from "lit/directives/class-map.js";
 
 interface TextfieldArgs {
   label: string;
@@ -7,6 +8,7 @@ interface TextfieldArgs {
   placeholder: string;
   disabled: boolean;
   readonly: boolean;
+  round: boolean;
 }
 
 const meta: Meta<TextfieldArgs> = {
@@ -16,9 +18,15 @@ const meta: Meta<TextfieldArgs> = {
     placeholder: "Placeholder",
     disabled: false,
     readonly: false,
+    round: false,
   },
   render: (args) => html`
-    <div class="hap-textfield">
+    <div
+      class=${classMap({
+        "hap-textfield": true,
+        "hap-textfield-round": args.round,
+      })}
+    >
       <label for="textfield">${args.label}</label>
       <input
         id="textfield"
@@ -36,3 +44,34 @@ export default meta;
 type Story = StoryObj<TextfieldArgs>;
 
 export const Default: Story = {};
+
+export const Round: Story = { args: { round: true } };
+
+export const Readonly: Story = { args: { readonly: true } };
+
+export const Disabled: Story = { args: { disabled: true } };
+
+export const Invalid: Story = {
+  render: (args) => html`
+    <div
+      class=${classMap({
+        "hap-textfield": true,
+        "hap-tone-invalid": true,
+        "hap-textfield-round": args.round,
+      })}
+    >
+      <label for="textfield">
+        <span class="placeholder-icon">[ERR]</span>
+        ${args.label}
+      </label>
+      <input
+        id="textfield"
+        aria-describedby="description"
+        placeholder=${args.placeholder}
+        ?disabled=${args.disabled}
+        ?readonly=${args.readonly}
+      />
+      <span id="description">${args.description}</span>
+    </div>
+  `,
+};
