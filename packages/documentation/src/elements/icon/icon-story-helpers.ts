12b4b424e3e37f56e29a5fe3910f4687d5bc1258
Fabian Gaukler <fabian.gaukler@holisticon.de>
2025-02-07 14:05:12 +0100

    chore(app): simplifies icon rendering

--- packages/documentation/src/elements/icon/icon-story-helpers.ts
+++ packages/documentation/src/elements/icon/icon-story-helpers.ts
@@ -1,14 +1,11 @@
 import type { ArgTypes } from "@storybook/web-components";
-import { html } from "lit";
 
 export const iconArgs = (
-  iconNames: string[],
   size: string,
   // default values for stroke according to design
   strokeWidth = 1.5,
   strokeOpacity = 0.8,
 ): IconArgs => ({
-  iconNames,
   size,
   strokeWidth,
   strokeOpacity,
@@ -22,15 +19,10 @@ export const iconArgTypes: Partial<ArgTypes<IconArgs>> = {
 };
 
 export interface IconArgs {
-  iconNames: string[];
   size: string;
   strokeWidth: number;
   strokeOpacity: number;
 }
-
-export const renderIcons = (args: IconArgs) => html`
-  ${args.iconNames.map((iconName) => html`<i data-lucide=${iconName}></i>`)}
-`;
 
 export const mapIconSizeToPx = (size: string): string => {
   if (size === "small") {
--- packages/documentation/src/elements/icon/icon.stories.ts
+++ packages/documentation/src/elements/icon/icon.stories.ts
@@ -5,63 +5,58 @@ import {
   iconArgs,
   iconArgTypes,
   mapIconSizeToPx,
-  renderIcons,
   type IconArgs,
 } from "./icon-story-helpers.js";
-
-const iconList = [
-  "check",
-  "arrow-right",
-  "arrow-left",
-  "arrow-down",
-  "arrow-up",
-  "arrow-up-right",
-  "arrow-down-right",
-  "arrow-down-left",
-  "arrow-down-up",
-  "circle-x",
-  "chevron-down",
-  "chevron-up",
-  "chevron-left",
-  "chevron-right",
-  "plus",
-  "log-out",
-  "shopping-cart",
-  "user-round",
-  "clock",
-  "menu",
-  "search",
-  "eye",
-  "circle-alert",
-  "code",
-  "copy",
-  "circle-play",
-  "circle-help",
-  "settings",
-  "bell",
-  "building-2",
-  "file",
-  "calendar",
-  "trash-2",
-  "eye-off",
-  "sparkles",
-  "info",
-  "megaphone",
-  "circle-check",
-  "folder-open",
-  "mail",
-  "download",
-  "lock",
-];
 
 const meta: Meta<IconArgs> = {
   argTypes: iconArgTypes,
 
-  render: (args) =>
+  render: () =>
     html`<div
       style="display:grid;grid-template-columns: repeat(14, max-content);gap:40px"
     >
-      ${renderIcons(args)}
+      <i data-lucide="check"></i>
+      <i data-lucide="arrow-right"></i>
+      <i data-lucide="arrow-left"></i>
+      <i data-lucide="arrow-down"></i>
+      <i data-lucide="arrow-up"></i>
+      <i data-lucide="arrow-up-right"></i>
+      <i data-lucide="arrow-down-right"></i>
+      <i data-lucide="arrow-down-left"></i>
+      <i data-lucide="arrow-down-up"></i>
+      <i data-lucide="circle-x"></i>
+      <i data-lucide="chevron-down"></i>
+      <i data-lucide="chevron-up"></i>
+      <i data-lucide="chevron-left"></i>
+      <i data-lucide="chevron-right"></i>
+      <i data-lucide="plus"></i>
+      <i data-lucide="log-out"></i>
+      <i data-lucide="shopping-cart"></i>
+      <i data-lucide="user-round"></i>
+      <i data-lucide="clock"></i>
+      <i data-lucide="menu"></i>
+      <i data-lucide="search"></i>
+      <i data-lucide="eye"></i>
+      <i data-lucide="circle-alert"></i>
+      <i data-lucide="code"></i>
+      <i data-lucide="copy"></i>
+      <i data-lucide="circle-play"></i>
+      <i data-lucide="circle-help"></i>
+      <i data-lucide="settings"></i>
+      <i data-lucide="bell"></i>
+      <i data-lucide="building-2"></i>
+      <i data-lucide="file"></i>
+      <i data-lucide="calendar"></i>
+      <i data-lucide="trash-2"></i>
+      <i data-lucide="eye-off"></i>
+      <i data-lucide="sparkles"></i>
+      <i data-lucide="info"></i>
+      <i data-lucide="megaphone"></i>
+      <i data-lucide="circle-check"></i>
+      <i data-lucide="folder-open"></i>
+      <i data-lucide="mail"></i>
+      <i data-lucide="download"></i>
+      <i data-lucide="lock"></i>
     </div>`,
   play: (args) => {
     createIcons({
@@ -71,8 +66,6 @@ const meta: Meta<IconArgs> = {
         "stroke-opacity": args.initialArgs.strokeOpacity,
         height: mapIconSizeToPx(args.initialArgs.size),
         width: mapIconSizeToPx(args.initialArgs.size),
-        // TODO should stroke color be declared in CSS directly?
-        color: "#141419CC",
       },
     });
   },
@@ -82,5 +75,5 @@ export default meta;
 
 export type Story = StoryObj<IconArgs>;
 
-export const IconsDefault: Story = { args: iconArgs(iconList, "default") };
-export const IconsSmall: Story = { args: iconArgs(iconList, "small") };
+export const IconsDefault: Story = { args: iconArgs("default") };
+export const IconsSmall: Story = { args: iconArgs("small") };
