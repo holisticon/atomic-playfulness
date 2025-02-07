c437b582e708c9882c730ada78335055b132c002
Fabian Gaukler <fabian.gaukler@holisticon.de>
2025-02-07 14:05:53 +0100

    feat: ensures icons are always rendered in play step in storybook

--- packages/documentation/.storybook/preview.ts
+++ packages/documentation/.storybook/preview.ts
@@ -1,5 +1,6 @@
 import "@holisticon/hap-foundation/atomic-playfulness.css";
 import type { Preview } from "@storybook/web-components";
+import { createIcons, icons } from "lucide";
 import { holisticonTheme } from "./theme.js";
 
 const preview: Preview = {
@@ -11,6 +12,24 @@ const preview: Preview = {
       },
     },
   },
+  decorators: [
+    (storyFn) => {
+      const story = storyFn(); // Render the story
+
+      // Ensure icons are initialized after rendering
+      setTimeout(() => {
+        createIcons({
+          icons,
+          attrs: {
+            "stroke-width": 1.5,
+            "stroke-opacity": 0.8,
+          },
+        });
+      }, 0);
+
+      return story;
+    },
+  ],
 };
 
 export default preview;
