7713d98e69c89ccc65fe0d8de4001e238fa087be
Fabian Gaukler <fabian.gaukler@holisticon.de>
2025-02-21 11:53:23 +0100

    feat(app): uses icons in badge stories

--- packages/documentation/src/elements/badge/badge.stories.ts
+++ packages/documentation/src/elements/badge/badge.stories.ts
@@ -1,5 +1,14 @@
 import type { Meta, StoryObj } from "@storybook/web-components";
 import { html } from "lit";
+import {
+  ArrowDown,
+  ArrowUpRight,
+  Check,
+  CircleAlert,
+  createIcons,
+  Info,
+  Sparkles,
+} from "lucide";
 
 type BadgeVariant =
   | "default"
@@ -19,7 +28,6 @@ const meta: Meta<BadgeArgs> = {
   args: {
     label: "Badge",
     variant: "default",
-    icon: "[Icon]",
   },
   argTypes: {
     variant: {
@@ -32,37 +40,50 @@ const meta: Meta<BadgeArgs> = {
     <div style="display:flex;gap:0.5rem;flex-direction:column;padding:2rem">
       <div class="hap-badge regular ${args.variant}">
         <span>${args.label}</span>
-        <span class="icon">${args.icon}</span>
+        ${args.icon ? html`<i data-lucide=${args.icon}></i>` : ""}
       </div>
       <div class="hap-badge bold ${args.variant}">
         <span>${args.label}</span>
-        <span class="icon">${args.icon}</span>
+        ${args.icon ? html`<i data-lucide=${args.icon}></i>` : ""}
       </div>
     </div>
   `,
+  play: () =>
+    createIcons({
+      icons: {
+        Info,
+        ArrowUpRight,
+        CircleAlert,
+        ArrowDown,
+        Check,
+        Sparkles,
+      },
+    }),
 };
 
 export default meta;
 type Story = StoryObj<BadgeArgs>;
 
 export const Default: Story = {
-  args: { variant: "default", label: "Default" },
+  args: { variant: "default", label: "Default", icon: "info" },
 };
 
 export const Positive: Story = {
-  args: { variant: "positive", label: "Positive" },
+  args: { variant: "positive", label: "Positive", icon: "arrow-up-right" },
 };
 
 export const Caution: Story = {
-  args: { variant: "caution", label: "Caution" },
+  args: { variant: "caution", label: "Caution", icon: "circle-alert" },
 };
 
 export const Critical: Story = {
-  args: { variant: "critical", label: "Critical" },
+  args: { variant: "critical", label: "Critical", icon: "arrow-down" },
 };
 
 export const Neutral: Story = {
-  args: { variant: "neutral", label: "Neutral" },
+  args: { variant: "neutral", label: "Neutral", icon: "check" },
 };
 
-export const New: Story = { args: { variant: "new", label: "New" } };
+export const New: Story = {
+  args: { variant: "new", label: "New", icon: "sparkles" },
+};
