c188d99f070efd0aba3ced745c01a17456f4fc20
Lukas Taake <lukas.taake@holisticon.de>
2025-03-07 16:41:25 +0100

    feat: add snackbar

---
+++ packages/documentation/src/elements/snackbar/snackbar.stories.ts
@@ -0,0 +1,52 @@
+import type { Meta, StoryObj } from "@storybook/web-components";
+import { html } from "lit";
+
+type SnackbarVariant = "hap-snackbar" | "hap-snackbar-multiline";
+
+interface AlertArgs {
+  label: string;
+  variant: SnackbarVariant;
+  icon?: string;
+  iconbutton?: string;
+}
+
+const meta: Meta<AlertArgs> = {
+  args: {
+    label: "Snackbar text",
+    icon: "info",
+    iconbutton: "circle-x",
+  },
+  // TODO: use actual icon button for cancel
+  render: (args) => html`
+    <div style="width: 374px;">
+      <div class="${args.variant}">
+        <div class="hap-snackbar-content">
+          <span>${args.label}</span>
+          <div class="hap-snackbar-actions">
+            <button class="hap-snackbar-button">Action</button>
+            ${args.iconbutton
+              ? html`<i
+                  class="hap-iconbutton"
+                  data-lucide=${args.iconbutton}
+                ></i>`
+              : ""}
+          </div>
+        </div>
+      </div>
+    </div>
+  `,
+};
+
+export default meta;
+type Story = StoryObj<AlertArgs>;
+
+export const SingleLine: Story = {
+  args: { variant: "hap-snackbar", label: "This is a single line snackbar." },
+};
+
+export const MultiLine: Story = {
+  args: {
+    variant: "hap-snackbar-multiline",
+    label: "This is a very long multi line snackbar that spans multiple lines.",
+  },
+};
