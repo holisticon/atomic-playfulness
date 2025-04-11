22ec1390df2bcab4f30ea373cdbf8bf7e3806d36
Fabian Gaukler <fabian.gaukler@holisticon.de>
2025-04-11 17:01:55 +0200

    chore: uses classmap for styling

--- packages/documentation/src/elements/divider/divider.stories.ts
+++ packages/documentation/src/elements/divider/divider.stories.ts
@@ -1,5 +1,6 @@
 import type { Meta, StoryObj } from "@storybook/web-components";
 import { html } from "lit";
+import { classMap } from "lit/directives/class-map.js";
 import { styleMap } from "lit/directives/style-map.js";
 
 type DividerOrientation = "horizontal" | "vertical";
@@ -31,41 +32,55 @@ const meta: Meta<DividerArgs> = {
       <div
         style=${styleMap({
           display: "flex",
-          gap: "0.5rem",
+          gap: "1rem",
           width: args.orientation === "horizontal" ? "100%" : "auto",
           flexDirection: args.orientation === "horizontal" ? "column" : "row",
           padding: "2rem",
           height: args.orientation === "horizontal" ? "100px" : "200px", // arbitrary sizes
         })}
       >
-        <!-- TODO we only need the class with orientation when using vertical divider -->
-        <hr class="hap-divider hap-divider-${args.orientation}" />
         <hr
-          class="hap-divider hap-divider-${args.orientation} hap-divider--inset"
+          class="hap-divider ${classMap({
+            "hap-divider-vertical": args.orientation === "vertical",
+          })}"
         />
         <hr
-          class="hap-divider hap-divider-${args.orientation} hap-divider--middle-inset"
+          class="hap-divider hap-divider--inset ${classMap({
+            "hap-divider-vertical": args.orientation === "vertical",
+          })}"
+        />
+        <hr
+          class="hap-divider hap-divider--middle-inset ${classMap({
+            "hap-divider-vertical": args.orientation === "vertical",
+          })}"
         />
       </div>
       <div
         style=${styleMap({
           display: "flex",
-          gap: "0.5rem",
+          gap: "1rem",
           width: args.orientation === "horizontal" ? "100%" : "auto",
           flexDirection: args.orientation === "horizontal" ? "column" : "row",
           padding: "2rem",
           height: args.orientation === "horizontal" ? "100px" : "200px", // arbitrary sizes
         })}
       >
-        <!-- TODO we only need the class with orientation when using vertical divider -->
         <hr
-          class="hap-divider hap-divider-bold hap-divider-${args.orientation}"
+          class="hap-divider hap-divider-bold ${classMap({
+            "hap-divider-vertical": args.orientation === "vertical",
+          })}"
         />
         <hr
-          class="hap-divider hap-divider-bold hap-divider-${args.orientation} hap-divider--inset"
+          class="hap-divider hap-divider-bold hap-divider--inset ${classMap({
+            "hap-divider-vertical": args.orientation === "vertical",
+          })}"
         />
         <hr
-          class="hap-divider hap-divider-bold hap-divider-${args.orientation} hap-divider--middle-inset"
+          class="hap-divider hap-divider-bold hap-divider--middle-inset ${classMap(
+            {
+              "hap-divider-vertical": args.orientation === "vertical",
+            },
+          )}"
         />
       </div>
     </div>
