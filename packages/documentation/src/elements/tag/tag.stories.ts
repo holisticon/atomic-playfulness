257edf8394d31485b4f2d8a507c6beed6e346df7
Christoph Fricke <christoph.fricke@holisticon.de>
2025-05-23 15:52:30 +0200

    feat(tag): add more docs for usage and leading icon example

--- packages/documentation/src/elements/tag/tag.stories.ts
+++ packages/documentation/src/elements/tag/tag.stories.ts
@@ -17,3 +17,14 @@ type Story = StoryObj<TagArgs>;
 export const Default: Story = {
   render: (args) => html`<span class="hap-tag">${args.label}</span>`,
 };
+
+export const LeadingIcon: Story = {
+  render: (args) => html`
+    <span class="hap-tag">
+      <svg viewBox="0 0 24 24" class="hap-icon hap-icon--small">
+        <use href="#sparkles"></use>
+      </svg>
+      ${args.label}
+    </span>
+  `,
+};
