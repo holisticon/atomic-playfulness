bcd6f8046d9c16fce599ffe924ab84e24d0f3453
Fabian Gaukler <fabian.gaukler@holisticon.de>
2025-05-23 16:57:19 +0200

    feat: implement navigation rail item

---
+++ packages/documentation/src/elements/navigation-rail-item/navigation-rail-item-story-helpers.ts
@@ -0,0 +1,37 @@
+import { html } from "lit";
+import { classMap } from "lit/directives/class-map.js";
+import { renderIcon } from "../icon/icon.stories.js";
+import {
+  renderIndicator,
+  type IndicatorVariant,
+} from "../indicator/indicator-story-helpers.js";
+
+export type BadgeVariant = IndicatorVariant | "none";
+
+export interface NavigationRailItemArgs {
+  selected?: boolean;
+  icon: string;
+  label?: string;
+  badgeVariant: BadgeVariant;
+  badgeCount?: string;
+}
+
+export const renderNavigationRailItem = (args: NavigationRailItemArgs) => html`
+  <a
+    class=${classMap({
+      "hap-navigation-rail-item": true,
+      "hap-navigation-rail-item--selected": args.selected ?? false,
+    })}
+    href="#"
+  >
+    <div class="hap-navigation-rail-item-icon">
+      ${renderIcon(args.icon)}
+      ${args.badgeVariant !== "none"
+        ? renderIndicator(args.badgeVariant, args.badgeCount)
+        : ""}
+    </div>
+    ${args.label
+      ? html`<div class="hap-navigation-rail-item-label">${args.label}</div>`
+      : ""}
+  </a>
+`;
