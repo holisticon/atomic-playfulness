ec2d3d55a3c05c61a548f13d62f351c833fb2d17
Christoph Fricke <christoph.fricke@holisticon.de>
2025-06-06 16:16:50 +0200

    feat: add option for closeable tags

--- packages/documentation/src/elements/tag/tag.stories.ts
+++ packages/documentation/src/elements/tag/tag.stories.ts
@@ -1,32 +1,43 @@
 import type { Meta, StoryObj } from "@storybook/web-components";
 import { html } from "lit";
+import { when } from "lit/directives/when.js";
 import { renderIcon } from "../icon/icon.stories.js";
 
 interface TagArgs {
   label: string;
+  withIcon?: boolean;
+  closeable?: boolean;
 }
 
 const meta: Meta<TagArgs> = {
   args: {
     label: "Tag Label",
+    withIcon: false,
+    closeable: false,
   },
+  render: (args) =>
+    html`<span class="hap-tag">
+      ${when(args.withIcon, () => renderIcon("sparkles", "small"))}
+      ${args.label}
+      ${when(
+        args.closeable,
+        () =>
+          html`<button class="hap-icon-button--xs">
+            ${renderIcon("circle-x", "small")}
+          </button>`,
+      )}
+    </span>`,
 };
 
 export default meta;
 type Story = StoryObj<TagArgs>;
 
-export const Default: Story = {
-  render: (args) => html`<span class="hap-tag">${args.label}</span>`,
-};
+export const Default: Story = {};
 
 export const LeadingIcon: Story = {
-  render: (args) => html`
-    <span class="hap-tag">
-      ${renderIcon("sparkles")}
-      <svg viewBox="0 0 24 24" class="hap-icon hap-icon--small">
-        <use href="#sparkles"></use>
-      </svg>
-      ${args.label}
-    </span>
-  `,
+  args: { withIcon: true },
+};
+
+export const Closeable: Story = {
+  args: { closeable: true },
 };
