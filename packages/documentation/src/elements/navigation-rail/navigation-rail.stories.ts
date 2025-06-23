9df7f1e87f97bc8e0d9c6ba5317565e377329d41
Fabian Gaukler <fabian.gaukler@holisticon.de>
2025-06-23 17:30:16 +0200

    feat(app): add navigation rail

---
+++ packages/documentation/src/elements/navigation-rail/navigation-rail.stories.ts
@@ -0,0 +1,31 @@
+import type { Meta, StoryObj } from "@storybook/web-components";
+import { html } from "lit";
+import {
+  renderNavigationRailItem,
+  type NavigationRailItemArgs,
+} from "./navigation-rail-item-story-helpers.js";
+
+const meta: Meta<NavigationRailItemArgs> = {
+  args: {
+    icon: "shopping-cart",
+    badgeCount: "3",
+    badgeVariant: "none",
+    label: "Label",
+  },
+  render: (args) =>
+    html`<div
+      style="display:flex;gap:0.5rem;padding:2rem;flex-direction:column"
+    >
+      ${renderNavigationRailItem({ ...args, icon: "menu", label: "" })}
+      ${renderNavigationRailItem({ ...args, selected: true })}
+      ${renderNavigationRailItem({ ...args, badgeVariant: "default" })}
+      ${renderNavigationRailItem(args)} ${renderNavigationRailItem(args)}
+      ${renderNavigationRailItem(args)}
+    </div>`,
+};
+
+export default meta;
+
+export type Story = StoryObj<NavigationRailItemArgs>;
+
+export const NavigationRail: Story = {};
