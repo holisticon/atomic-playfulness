81e5c1a3407e6251b24e777184957dd3fd0ea158
Christoph Fricke <christoph.fricke@holisticon.de>
2025-06-30 15:35:42 +0200

    feat(topbar): add topbar and nav-link styles (#144)

---
+++ packages/documentation/src/elements/topbar/topbar.stories.ts
@@ -0,0 +1,44 @@
+import type { Meta, StoryObj } from "@storybook/web-components";
+import { html } from "lit";
+import { renderIcon } from "../icon/icon.stories.js";
+
+const meta: Meta = {};
+
+export default meta;
+type Story = StoryObj;
+
+export const Default: Story = {
+  render: () =>
+    html`<div class="hap-topbar">
+      <div class="hap-headline-sm">FANCY LOGO</div>
+      <nav class="hap-topbar-nav">
+        <a class="hap-topbar-nav-item" href="#">Link 1</a>
+        <a class="hap-topbar-nav-item" href="#">Link 2</a>
+        <a class="hap-topbar-nav-item" href="#">Link 3</a>
+        <a class="hap-topbar-nav-item hap-topbar-nav-item--disabled" href="#">
+          Link 4
+        </a>
+        <a class="hap-topbar-nav-item" href="#">Link 5</a>
+      </nav>
+      <button class="hap-button hap-button--secondary hap-button--small ">
+        ${renderIcon("log-out")} Logout
+      </button>
+    </div>`,
+};
+
+export const Search: Story = {
+  render: () =>
+    html`<div class="hap-topbar">
+      <div class="hap-headline-sm">FANCY LOGO</div>
+      <div class="hap-search">
+        <span class="hap-search-icon">${renderIcon("search", "small")}</span>
+        <input id="search" arial-label="Search" placeholder="Placeholder" />
+        <button class="hap-icon-button--sm">
+          ${renderIcon("arrow-right", "small")}
+        </button>
+      </div>
+      <button class="hap-button hap-button--secondary hap-button--small ">
+        ${renderIcon("log-out")} Logout
+      </button>
+    </div>`,
+};
