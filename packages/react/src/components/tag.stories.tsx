acf9ca48d9402ab93ebb60663199406d49c034b0
Christoph Fricke <christoph.fricke@holisticon.de>
2025-08-19 14:17:03 +0200

    feat(react): add Tag component and story using cva

---
+++ packages/react/src/components/tag.stories.tsx
@@ -0,0 +1,15 @@
+import type { Meta, StoryObj } from "@storybook/react";
+import { Tag } from "./tag.js";
+
+const meta = {
+  component: Tag,
+  args: {
+    children: "Tag Label",
+  },
+} satisfies Meta<typeof Tag>;
+
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {};
