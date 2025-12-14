49e9b4821aa4c19c13e6d5db7b77e874d2aede68
Lukas Taake <lukas.taake@holisticon.de>
2025-12-14 18:15:05 +0100

    chore: use eslint-plugin-storybook

--- packages/documentation/eslint.config.mjs
+++ packages/documentation/eslint.config.mjs
@@ -1,5 +1,5 @@
 // For more info, see https://github.com/storybookjs/eslint-plugin-storybook#configuration-flat-config-format
-// import storybook from "eslint-plugin-storybook";
+import storybook from "eslint-plugin-storybook";
 
 import { defineConfig, globalIgnores } from "eslint/config";
 import config from "../../eslint.config.mjs";
@@ -7,4 +7,5 @@ import config from "../../eslint.config.mjs";
 export default defineConfig(
   globalIgnores(["test-results", "playwright-report", "!.storybook"]),
   config,
+  storybook.configs["flat/recommended"],
 );
